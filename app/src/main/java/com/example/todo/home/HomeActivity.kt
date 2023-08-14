bbd429d82fa21e4afb2db162dc4d09a3890ee6f3
mahmoud adel <mahmoudadel5556@gmail.com>
2023-08-14 22:52:31 +0300

    update code

--- app/src/main/java/com/example/todo/home/HomeActivity.kt
+++ app/src/main/java/com/example/todo/home/HomeActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.todo.home.ui.theme.ToDoTheme
+import com.example.todo.ui.theme.ToDoTheme
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
